92ad3c3994a731efe04ecb8005df5c411fc1ba40
Saravana <kumarkpsaravana@outlook.ie>
2021-04-04 10:23:46 +0530

    Added WEB API application

---
+++ RBWebAPI/RBWebAPI/Controllers/AdministratorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RBWebAPI.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdministratorController : ApiController
+    {
+        public string Get()
+        {
+            return "Sensitive Data";
+        }
+    }
+}
---
+++ RBWebAPI/RBWebAPI/Controllers/ClerkController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RBWebAPI.Controllers
+{
+
+    [Authorize(Roles = "Administrator,Clerk")]
+    public class ClerkController : ApiController
+    {
+        public string Get()
+        {
+            return "clerical/Common Data";
+        }
+    }
+}
